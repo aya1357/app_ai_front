68149267e90e15770ad1e8a2a815643fcc07c011
aya1357 <katoa5429@gmail.com>
2024-06-20 01:31:39 +0900

    flowbite-reactの導入

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import flowbite from 'flowbite-react/tailwind'
 import type { Config } from 'tailwindcss'
 
 const config = {
@@ -7,6 +8,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    flowbite.content(),
   ],
   prefix: '',
   theme: {
@@ -84,7 +86,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), flowbite.plugin()],
 } satisfies Config
 
 export default config
